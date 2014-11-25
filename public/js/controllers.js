dd09dc3316c2f0abb1eb55314d88f88a36332d28
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-25 20:20:08 +0000

    now functions with searching for repos, gets description and title

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,19 +1,29 @@
 var app = angular.module('githubsearch', []);
 
-app.controller('SearchController', function($scope) {
-})
 
-app.factory('Github', function($http) {
+
+app.controller('SearchController', ['$scope', 'Github', function SearchController($scope, Github) {
+    $scope.executeSearch = function executeSearch() {
+       Github.searchRepos($scope.query, function (error, data) {
+           if (!error) {
+               $scope.repos = data.items;
+           }
+       });
+    }
+}])
+
+app.factory('Github', function Github($http) {
     return {
+        searchRepos: function searchRepos(query, callback) {
+            $http.get('https://api.github.com/search/repositories', { params: { q: query } })
+                .success(function (data) {
+                    callback(null, data);
+                })
+                .error(function (e) {
+                    callback(e);
+                });
+        }
     };
 });
 
-searchRepos: function(query, callback){
-    $http.get('https://api.github.com/search/repositories', { params: {q: query} })
-        .success(function(data) {
-            callback(null, data);
-        })
-        .error(function(e) {
-            callback(e);
-        });
-}
+
