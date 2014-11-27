8d2187cc2dfe8de839aa68600d184a1675e7ffe2
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-27 10:24:49 +0000

    messing with callbacks

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,46 +1,63 @@
 var app = angular.module('githubsearch', []);
+var language_objects = new Array;
 
 app.controller('SearchController', ['$scope', 'Github', function SearchController($scope, Github) {
-    $scope.language_objects = new Array;
     $scope.executeSearch = function executeSearch() {
        Github.findUser($scope.query, function (error, data) {
            if (!error) {
                $scope.user = data;
                Github.getUrl(data.repos_url, function(error, data) {
-                   $scope.repos = data;
-                   $scope.repos.forEach(function(object, index) {
-                       var languages_url = object.languages_url
-                       Github.getUrl(languages_url, function(error, data) {
-                           $scope.language_objects.push(data);
-                       });
-                   });
+                   getRepos(data, Github, iterateRepos);
                });
            }
        });
     }
 }])
 
+function iterateRepos(object) {
+    console.log(object)
+}
+
+function getRepos(objects, Github, callback) {
+    var i = 0
+    function gitForLoop() {
+        if (i < objects.length) {
+            var languages_url = objects[i].languages_url
+            Github.getUrl(languages_url, function(error, data) {
+                language_objects.push(data)
+            });
+            i++
+                setTimeout(gitForLoop, 0)
+            callback(language_objects)
+        }
+    }
+    gitForLoop(function(){
+    });
+}
+
+
+
 app.factory('Github', function Github($http) {
     return {
         findUser: function findUser(query, callback) {
             $http.get('https://api.github.com/users/' + query )
-                .success(function (data) {
-                    callback(null, data);
-                })
-                .error(function (e) {
-                    callback(e);
-                });
+            .success(function (data) {
+                callback(null, data);
+            })
+            .error(function (e) {
+                callback(e);
+            });
         },
         getUrl: function getUrl(url, callback) {
             $http.get(url)
-                .success(function (data) {
-                    callback(null, data);
-                })
-                .error(function (e) {
-                    callback(e);
-                });
+            .success(function (data) {
+                callback(null, data);
+            })
+            .error(function (e) {
+                callback(e);
+            });
         }
-        
+
     };
 });
 
