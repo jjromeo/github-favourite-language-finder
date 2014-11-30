8322829e8358202a53623d288de55c94d4e878d3
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-30 23:39:46 +0000

    again trying to sort callback in angular but to no avail

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,22 +1,35 @@
 var app = angular.module('githubsearch', []);
 
-app.controller('SearchController', ['$scope', 'Github', function SearchController($scope, Github) {
+app.controller('SearchController', ['$scope','$q', 'Github', function SearchController($scope, $q, Github) {
     $scope.language_objects = new Array;
     $scope.executeSearch = function executeSearch() {
-       Github.findUser($scope.query, function (error, data) {
-           if (!error) {
-               $scope.user = data;
-               Github.getUrl(data.repos_url, function(error, data) {
-                   $scope.repos = data;
-                   $scope.repos.forEach(function(object, index) {
-                       var languages_url = object.languages_url
-                       Github.getUrl(languages_url, function(error, data) {
-                           $scope.language_objects.push(data);
+        $scope.grabLanguages = function(url, callback) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+            Github.findUser($scope.query, function (error, data) {
+               if (!error) {
+                   $scope.user = data;
+                   Github.getUrl(data.repos_url, function(error, data) {
+                       $scope.repos = data;
+                       promise.then(function() {
+                           $scope.repos.forEach(function(object, index) {
+                               var languages_url = object.languages_url
+                               Github.getUrl(languages_url, function(error, data) {
+                                   $scope.language_objects.push(data);
+                               });
+                           });
+                       }).then(function() {
+                           console.log($scope.language_objects)
+                           callback($scope.language_objects);
                        });
+                       deferred.resolve();
                    });
-               });
-           }
-       });
+               }
+           });
+        }
+        $scope.grabLanguages("bla-bla", function(objects) {
+            console.log(objects);
+        })
     }
 }])
 
