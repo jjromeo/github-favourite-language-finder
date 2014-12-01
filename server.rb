9b413c1a918d517f43ef1797ff3b51a73e747647
Jerome Pratt <jerome.pratt@outlook.com>
2014-12-01 01:23:53 +0000

    can now get array of hashes with languages for a users repos, now need to add testing and get fave

--- server.rb
+++ server.rb
@@ -10,17 +10,26 @@ end
 
 post '/languages' do 
     username = params["username"]
-    url = URI.parse("https://api.github.com/users/#{username}")
-    user_json = https_get_request(url)
-    repos_url = URI.parse(user_json["repos_url"])
-    user_repos = https_get_request(repos_url)
+    user_json = https_get_request("https://api.github.com/users/#{username}")
+    user_repos = https_get_request(user_json["repos_url"])
+    @languages_array = []
+    user_repos.each do |repo|
+        @languages_array << https_get_request(repo["languages_url"])
+    end
+    @languages = @languages_array.map {|hash| hash.keys.uniq}
+    puts @languages
     redirect to '/'
 end 
 
 def https_get_request(url)
+    url = URI.parse(url)
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     req = Net::HTTP::Get.new(url)
     res = http.request(req)
     json = JSON.parse(res.body)
+end
+
+def get_languages(url)
+
 end
