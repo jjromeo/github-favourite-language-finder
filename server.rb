d5c4042cdefab2a06b0a5eb69f98662603c6f57f
Jerome Pratt <jerome.pratt@outlook.com>
2014-12-01 14:54:27 +0000

    Added further tests and refactored server.rb

--- server.rb
+++ server.rb
@@ -5,21 +5,13 @@ require 'uri'
 require 'haml'
 require 'json'
 
-env = ENV['RACK_ENV'] || 'development'
 get '/' do 
     haml :index
 end
 
 post '/languages' do 
     username = params["username"]
-    user_json = https_get_request("https://api.github.com/users/#{username}")
-    user_repos = https_get_request(user_json["repos_url"])
-    @languages_array = []
-    user_repos.each do |repo|
-        @languages_array << https_get_request(repo["languages_url"])
-    end
-    @languages = @languages_array.map {|hash| hash.keys.uniq}
-    puts @languages
+    look_up_languages(username)
     redirect to '/'
 end 
 
@@ -32,7 +24,17 @@ def https_get_request(url)
     json = JSON.parse(res.body)
 end
 
-def get_languages(url)
+def get_languages(user_repos)
+    languages_array = Array.new
+    user_repos.each do |repo|
+        languages_array << https_get_request(repo["languages_url"])
+    end
+    languages_array
+end
 
+def look_up_languages(username)
+    user_json = https_get_request("https://api.github.com/users/#{username}")
+    user_repos = https_get_request(user_json["repos_url"])
+    get_languages(user_repos)
 end
 
--- spec/github_connect_spec.rb
+++ spec/github_connect_spec.rb
@@ -7,4 +7,21 @@ describe 'github_connect' do
             expect(user_json["login"]).to eq "jjromeo"
         end
     end
+
+    it 'can get to json of users repo from user\'s github profile' do
+        VCR.use_cassette('repos') do 
+            user_json = https_get_request('https://api.github.com/users/jjromeo')
+            repos_json = https_get_request(user_json["repos_url"])
+            expect(repos_json.class).to eq Array
+            expect(repos_json.count).to eq 30
+            expect(repos_json[0]["name"]).to eq "advanced-student-directory"
+        end
+    end
+
+    it 'can create an array of hashes of the languages a user uses' do 
+        VCR.use_cassette('languages') do 
+            expect(look_up_languages("jjromeo").class).to eq Array
+        end
+    end
+
 end
